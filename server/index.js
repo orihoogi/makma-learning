8e3186db00e679406ae98d97c7d7f5f64a2a323c
omrina <omri.nathanson@gmail.com>
2020-11-04 23:09:19 +0200

    feat(server-client-prod): make server serve client static files

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/', express.static('../client/dist'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 import authRoutes from "./routes/authRoutes";
@@ -25,3 +25,5 @@ photoRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+console.log(`server runs at port: ${PORT}`)
