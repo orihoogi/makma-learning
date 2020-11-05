86b7ccc595bc04c3ebf9e30a6e31ac2976432903
omrina <omri.nathanson@gmail.com>
2020-11-05 14:20:56 +0200

    z

--- server/services/ScriptsAndML_related/train_specific_folder.py
+++ server/services/ScriptsAndML_related/train_specific_folder.py
@@ -6,5 +6,5 @@ from imageai.Prediction.Custom import ModelTraining
 
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory("idenprof")
-model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.setDataDirectory("omri")
+model_trainer.trainModel(num_objects=2, num_experiments=1, enhance_data=True, batch_size=3, show_network_summary=True)
