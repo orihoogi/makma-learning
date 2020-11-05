f48c63bcac390d3b7098cd81f6465e4b6076d460
Ori Hoogi <ohoogi@gmail.com>
2020-11-05 15:15:53 +0200

    fix(bugs)

--- server/services/classify.js
+++ server/services/classify.js
@@ -4,20 +4,20 @@ const py = require('python-shell');
 
 const options = {
     mode: 'text',
-    pythonPath: 'C:\\Program Files\\Python36\\python.exe',
+    pythonPath: 'C:\\Users\\ori\\AppData\\Local\\Programs\\Python\\Python36\\python.exe',
 };
 
 const getOptions = req => {
-    // console.log(process.cwd())
     options.args = [`${process.cwd()}/images/${req.user.username}/test.jpg`, req.user.username];
 
     return options;
 }
 
-export const run = (req, res) => py.PythonShell.run(`${__dirname}/ScriptsAndML_related/classify_specific_image.py`, getOptions(req), function (err, results) {
-    console.log("wowow");
-    if (err) res.status(500).send(err);
-    // results is an array consisting of messages collected during execution
-    console.log(results);
-    // res.send(results[results.length - 1]);
-});
+export const run = (req, res) => {
+    py.PythonShell.run(`${__dirname}/ScriptsAndML_related/classify_specific_image.py`, getOptions(req), function (err, results) {
+        if (err) res.status(500).send(err);
+        // results is an array consisting of messages collected during execution
+        console.log(results);
+        res.send({});
+    });
+};
