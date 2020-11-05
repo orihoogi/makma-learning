ca2e0b854a0866f329ba1c674be28c3cbb24bb2b
omrina <omri.nathanson@gmail.com>
2020-11-05 14:50:14 +0200

    as

--- server/services/ScriptsAndML_related/classify_specific_image.py
+++ server/services/ScriptsAndML_related/classify_specific_image.py
@@ -3,12 +3,13 @@ import os, sys
 from imageai.Prediction.Custom import CustomImagePrediction
 
 def main(imgPath, username):
+    cwd = os.getcwd()
     model_trainer = ModelTraining()
     model_trainer.setModelTypeAsResNet()
-    model_trainer.setDataDirectory(os.path.join(cwd, "services/images/{username}))
+    model_trainer.setDataDirectory(os.path.join(cwd, "services/images/{username}"))
     model_trainer.trainModel(num_objects=2, num_experiments=1, enhance_data=True, batch_size=3, show_network_summary=True)
 
-    cwd = os.getcwd()
+
     prediction = CustomImagePrediction()
     prediction.setModelPath(model_path=os.path.join(cwd, "services/images/{username}/models/model_ex-001_acc-0.500000.h5"))
     prediction.setJsonPath(model_json=os.path.join(cwd, "services/images/{username}/json/model_class.json"))
--- server/services/classify.js
+++ server/services/classify.js
@@ -8,15 +8,16 @@ const options = {
 };
 
 const getOptions = req => {
-    console.log(process.cwd())
+    // console.log(process.cwd())
     options.args = [`${process.cwd()}/images/${req.user.username}/test.jpg`, req.user.username];
 
     return options;
 }
 
 export const run = (req, res) => py.PythonShell.run(`${__dirname}/ScriptsAndML_related/classify_specific_image.py`, getOptions(req), function (err, results) {
+    console.log("wowow")
     if (err) res.status(500).send(err);
     // results is an array consisting of messages collected during execution
-    console.log(results);
-    res.send(results);
+    console.log(results[results.length - 1]);
+    res.send(results[results.length - 1]);
 });
