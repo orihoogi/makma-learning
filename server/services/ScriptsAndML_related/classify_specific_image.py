0f4a0220d0fa1a131812ea4b57661584080c0195
Ori Hoogi <ohoogi@gmail.com>
2020-11-05 15:22:56 +0200

    wip

--- server/services/ScriptsAndML_related/classify_specific_image.py
+++ server/services/ScriptsAndML_related/classify_specific_image.py
@@ -1,5 +1,5 @@
 from imageai.Prediction.Custom import ModelTraining
-import os, sys
+import os, sys, shutil
 from imageai.Prediction.Custom import CustomImagePrediction
 
 def main(imgPath, username):
@@ -12,7 +12,8 @@ def main(imgPath, username):
     model_trainer.trainModel(num_objects=2, num_experiments=1, enhance_data=True, batch_size=3, show_network_summary=False)
 
     prediction = CustomImagePrediction()
-    prediction.setModelPath(model_path=os.path.join(cwd, "images\\" + username + "\\models\\model_ex-001_acc-0.500000.h5"))
+    model_name = os.listdir(os.path.join(cwd, "images\\" + username + "\\models\\"))[0];
+    prediction.setModelPath(model_path=os.path.join(cwd, "images\\" + username + "\\models\\" + model_name))
     prediction.setJsonPath(model_json=os.path.join(cwd, "images\\" + username + "\\json\\model_class.json"))
     prediction.setModelTypeAsResNet()
     prediction.loadModel(num_objects=2)
@@ -22,6 +23,10 @@ def main(imgPath, username):
     for eachPrediction, eachProbability in zip(predictions, probabilities):
         # res = res, eachPrediction, " : ", eachProbability
         print(eachPrediction, " : ", eachProbability)
+
+    shutil.rmtree(os.path.join(cwd, "images\\" + username + "\\models))
+    shutil.rmtree(os.path.join(cwd, "images\\" + username + "\\logs))
+    shutil.rmtree(os.path.join(cwd, "images\\" + username + "\\json))
         
 if __name__ == "__main__":
     imgPath = sys.argv[1]
