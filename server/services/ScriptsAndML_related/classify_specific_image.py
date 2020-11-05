13290228582a7cc025fcbfae1a6fbd80e8c4071b
omrina <omri.nathanson@gmail.com>
2020-11-05 13:12:45 +0200

    feat(generic-image_classify): the script works for classifying the user's image

--- server/services/ScriptsAndML_related/classify_specific_image.py
+++ server/services/ScriptsAndML_related/classify_specific_image.py
@@ -1,22 +1,26 @@
 from imageai.Prediction.Custom import ModelTraining
 import os, sys
 from imageai.Prediction.Custom import CustomImagePrediction
-det main():
-    execution_path = os.getcwd()
+
+def main(imgPath):
+    cwd = os.getcwd()
     prediction = CustomImagePrediction()
     print("wowowow")
     # prediction.setModelPath(modelPath)
-    prediction.setModelPath(model_path="../idenprof_resnet.h5"))
+    prediction.setModelPath(model_path=os.path.join(cwd, "services/ScriptsAndML_related/idenprof_resnet.h5"))
     # prediction.setJsonPath(jsonPath)
-    prediction.setJsonPath(model_json="../idenprof.json"))
+    prediction.setJsonPath(model_json=os.path.join(cwd, "services/ScriptsAndML_related/idenprof.json"))
     prediction.setModelTypeAsResNet()
     prediction.loadModel(num_objects=10)
-    predictions, probabilities = prediction.predictImage(sys.argv[1], result_count=3)
+    # predictions, probabilities = prediction.predictImage(os.path.join(cwd, "images/omri/test.jpg"), result_count=3)
+    predictions, probabilities = prediction.predictImage(imgPath, result_count=3)
 
     # res = None
 
     for eachPrediction, eachProbability in zip(predictions, probabilities):
         # res = res, eachPrediction, " : ", eachProbability
         print(eachPrediction, " : ", eachProbability)
+        
 if __name__ == "__main__":
+    print(sys.argv)
     main(sys.argv[1])
--- server/services/classify.js
+++ server/services/classify.js
@@ -1,16 +1,20 @@
-const PythonShell = require('python-shell');
+const py = require('python-shell');
+
+// require('../images/')
 
 const options = {
     mode: 'text',
-    pythonPath: 'path/to/python',
-    pythonOptions: ['-u'],
+    pythonPath: 'C:\\Program Files\\Python36\\python.exe',
 };
 
-const getOptions = photoLocation => {
-    options.args = [photoLocation]
+const getOptions = req => {
+    console.log(process.cwd())
+    options.args = [`${process.cwd()}/images/${req.user.username}/test.jpg`];
+
+    return options;
 }
 
-export const run = req => PythonShell.run('../../ScriptsAndML_related/classify_specific_image.py', getOptions(`./images/${req.user.username}/test.jpg`), function (err, results) {
+export const run = req => py.PythonShell.run(`${__dirname}/ScriptsAndML_related/classify_specific_image.py`, getOptions(req), function (err, results) {
     if (err) throw err;
     // results is an array consisting of messages collected during execution
     console.log(results);
